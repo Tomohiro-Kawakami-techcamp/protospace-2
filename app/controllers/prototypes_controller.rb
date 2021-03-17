04fd215bb942dc601c8df61e5ab2cdd71695d8b8
Tomohiro-Kawakami-techcamp <zhikuanchuanshang725@gmail.com>
2021-03-17 11:29:10 +0900

    全て終了

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,7 @@
 class PrototypesController < ApplicationController
+  before_action :authenticate_user!,  except: [:index, :show]
+  before_action :move_to_index, only: :edit
+
   def index
     @prototypes = Prototype.all
   end
@@ -18,6 +21,8 @@ class PrototypesController < ApplicationController
 
   def show
     @prototype = Prototype.find(params[:id])
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
   end
 
   def edit
@@ -35,10 +40,24 @@ class PrototypesController < ApplicationController
   end
 
   def destroy
+    prototype = Prototype.find(params[:id])
+    if prototype.destroy
+        redirect_to prototypes_path
+    else
+        render :edit
+    end
   end
 
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @prototype = Prototype.find(params[:id])
+    
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 end
